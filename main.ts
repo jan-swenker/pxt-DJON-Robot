c3efa5897b9ab5d38acc210a2a8c4776b45986f1
jan-swenker <104730685+jan-swenker@users.noreply.github.com>
2025-06-25 12:08:17 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+/**
+ * Robot extension voor DJON 
+ * Maakt het gemakkelijker om de robot te programmeren
+ * Linkerarm op  P2
+ * Rechterarm op P0
+ * Hoofd op      P1
+ */
+enum blik {
+    links = 0,
+    rechts = 180,
+    vooruit = 90
+}
+
+enum richting {
+    
+}
